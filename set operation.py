b1ce8afb27285ef4c93bcc3678e8c02cbecfb81b
VENGALA MANOGNA <86078373+manu07-oss@users.noreply.github.com>
2022-05-12 22:32:15 +0530

    Add files via upload

---
+++ set operation.py
@@ -0,0 +1,14 @@
+# set operation 
+# two set variable , union , intersection, differnces, symmetric differenc
+E = {0, 1, 2, 3}
+N = {4, 5, 6, 8}
+print(E|N) 
+E = {0,4,2,3}
+N = {4,2,6,8}
+print(E&N)
+E = {0, 4, 2, 3}
+N = {4,2,6,8}
+print(E-N)
+E = {0, 4, 2, 3}
+N = {4,2,6,8}
+print(E^N)
